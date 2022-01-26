27fd6364153cc4d8b0e78ba21f18cfc69b4f522c
Asmith18 <Smittysadam@gmail.com>
2022-01-26 17:51:16 -0600

    first commit

---
+++ SportsTeams/SportsTeams/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsTeams
+//
+//  Created by adam smith on 1/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
